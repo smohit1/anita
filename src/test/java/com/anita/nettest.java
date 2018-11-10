005c704c9b027ba780508dd548252a588db905bf
Mohit Sharma <sharma-mohit1@hotmail.com>
2018-11-10 13:36:39 +0530

    Fourth commit

---
+++ src/test/java/com/anita/nettest.java
@@ -0,0 +1,23 @@
+package com.anita;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+import junit.framework.Testcase;
+
+public class nettest extends Testcase
+{
+	
+     Calculator cal=new Calculator();
+
+     public nettest(String name) {
+	     super(name);
+
+	     public void testSum() {
+		     assertEquals(2,cal.sum(1,1));
+	     }
+     }
+
+    
